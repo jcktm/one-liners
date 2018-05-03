d0ec70d2020ddf97eaa6026e93e658c66b1c728d
Jack Murray <jack@murray.cx>
2018-05-03 20:46:21 +1000

    terminal - added -y flag to apt-get install

--- setup-terminal.sh
+++ setup-terminal.sh
@@ -6,7 +6,7 @@ then
   exit
 fi
 
-sudo apt-get install curl zsh wget
+sudo apt-get install -y curl zsh wget
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo 'PROMPT="%{$fg[blue]%}$USER%{$reset_color%}@%{$fg[blue]%}%M %{$fg_bold[red]%}➜ %{$fg_bold[green]%}%p %{$fg[cyan]%}%c %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%}%{$reset_color%}"' >> ~/.zshrc
 
