71d4f8a0be097b8a97f73bb0643ceac6dc9f2586
Jack Murray <jack@murray.cx>
2018-05-03 20:44:12 +1000

    terminal - added vimrc and zshrc

---
+++ setup-terminal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -f ~/.zshrc ]
+then
+  echo .zshrc exists, exiting.
+  exit
+fi
+
+sudo apt-get install curl zsh wget
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo 'PROMPT="%{$fg[blue]%}$USER%{$reset_color%}@%{$fg[blue]%}%M %{$fg_bold[red]%}➜ %{$fg_bold[green]%}%p %{$fg[cyan]%}%c %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%}%{$reset_color%}"' >> ~/.zshrc
+
+wget https://raw.githubusercontent.com/jcktm/one-liners/master/.vimrc
