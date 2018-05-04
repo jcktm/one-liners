ee51126abe614957dfa2e7b89ae2506d9807240a
jcktm <kitten@protonmail.com>
2018-05-04 14:37:13 +1000

    terminal - fixed git info

--- setup-terminal.sh
+++ setup-terminal.sh
@@ -8,7 +8,7 @@ fi
 
 sudo apt-get install -y curl zsh wget
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo 'PROMPT="$USER%{$fg[blue]%}@%{$reset_color%}%M %{$fg_bold[red]%}➜ %{$fg_bold[green]%}%p %{$fg[cyan]%}%c %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%}%{$reset_color%}"' >> ~/.zshrc
+echo PROMPT=\''$USER%{$fg[blue]%}@%{$reset_color%}%M %{$fg_bold[red]%}➜ %{$fg_bold[green]%}%p %{$fg[cyan]%}%c %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%}%{$reset_color%}'\' >> ~/.zshrc
 
 wget https://raw.githubusercontent.com/jcktm/one-liners/master/.vimrc
 
